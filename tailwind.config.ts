42bde14973f786fca9cf0dba1c60b61d17143c71
westofsky <westofsky159@gmail.com>
2023-12-05 15:37:09 +0900

    setting: default breakpoint 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,12 @@ const config: Config = {
 					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
 			},
 		},
+		screens: {
+			sm: '640px',
+			md: '920px',
+			lg: '1024px',
+			xl: '1280px',
+		},
 	},
 	plugins: [],
 };
