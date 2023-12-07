1d410236e68a831ad07343b0676534756cacb18b
ByteAurora <byteaurora.dev@gmail.com>
2023-12-07 14:03:30 +0900

    feat: Add orange color in theme-colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,43 +1,44 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
 const pxToRem = (px: number, base = 16) => `${px / base}rem`;
 const range = (start: number, end: number): number[] =>
-	Array.from({ length: end - start + 1 }, (_, index) => start + index);
+  Array.from({ length: end - start + 1 }, (_, index) => start + index);
 
 const config: Config = {
-	content: [
-		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
-		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
-	],
-	theme: {
-		extend: {
-			backgroundImage: {
-				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-				'gradient-conic':
-					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-			},
-			colors: {
-				'color-214': 'rgb(214,214,214)',
-				'color-112': 'rgb(112,112,112)',
-				'color-194': 'rgb(194,194,194)',
-			},
-			spacing: Array.from({ length: 1000 }, (_, index) => {
-				const value = pxToRem(index + 1);
-				return { [`${index + 1}pxr`]: value };
-			}).reduce((acc, obj) => ({ ...acc, ...obj }), {}),
-			fontSize: Array.from({ length: 1000 }, (_, index) => {
-				const value = pxToRem(index + 1);
-				return { [`${index + 1}pxr`]: value };
-			}).reduce((acc, obj) => ({ ...acc, ...obj }), {}),
-		},
-		screens: {
-			sm: '640px',
-			md: '920px',
-			lg: '1024px',
-			xl: '1280px',
-		},
-	},
-	plugins: [],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        "color-214": "rgb(214,214,214)",
+        "color-112": "rgb(112,112,112)",
+        "color-194": "rgb(194,194,194)",
+        "color-orange": "rgb(255,173,10)",
+      },
+      spacing: Array.from({ length: 1000 }, (_, index) => {
+        const value = pxToRem(index + 1);
+        return { [`${index + 1}pxr`]: value };
+      }).reduce((acc, obj) => ({ ...acc, ...obj }), {}),
+      fontSize: Array.from({ length: 1000 }, (_, index) => {
+        const value = pxToRem(index + 1);
+        return { [`${index + 1}pxr`]: value };
+      }).reduce((acc, obj) => ({ ...acc, ...obj }), {}),
+    },
+    screens: {
+      sm: "640px",
+      md: "920px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+  },
+  plugins: [],
 };
 export default config;
